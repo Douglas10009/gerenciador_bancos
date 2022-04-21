1ec7799aec36409cdb3529f65c5624f6f572073d
Aleatoriedade Q <>
2022-04-21 22:53:17 +0000

    Initial commit

---
+++ Banco.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Banco {
+   public static void main(String[] args) {
+
+     int [] clientes = new int [];
+     float [] contas = new float[];
+     float  saldo; // criar dentro do case
+     float deposito;// criar dentro do case
+     //Possuir um menu e submenus utilizando switch case;
+     //O programa só poderá encerrar após o usuário informar;
+     //Utilizar vetores ou matrizes para registrar as informações. Não pode usar ArrayList ou outras classes de objetos Java;
+     //Utilizar estruturas de controle e repetição para realizar operações do banco;
+     // O usuário cliente deverá poder: sacar, depositar, transferir de uma conta para outra conta de outro cliente já cadastrado.
+//O sistema deve visualizar o saldo de cada cliente cadastrado.
+    
+
+     switch (menu){
+         case 1:
+         System.out.println("Saque" + (saldo - saque));
+           float saque = sc.nextFloat();
+                break;
+       case 2:
+         System.out.println("Deposito" + (saldo + deposito));
+                break;
+       case 3:
+         System.out.println("Transferencia");
+                break;
+         
+     }
+     
+     }
+}
