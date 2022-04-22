99ab465ba504bd53ca2f86d0d535445e09f9b4a9
Douglas10009 <douglasevertonev@gmail.com>
2022-04-21 21:16:58 -0300

    Formatação do código

--- Banco.java
+++ Banco.java
@@ -1,35 +1,128 @@
 import java.util.Scanner;
+
 public class Banco {
-   public static void main(String[] args) {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-    int [] clientes = new int []; //Tem que ter a quantidade para ser inicializada
-    float [] contas = new float[]; //Aq tb
-    float  saldo; // criar dentro do case
-    float deposito; // criar dentro do case
+    //int[] clientes = new int[]; // Tem que ter a quantidade para ser inicializada
+    //float[] contas = new float[]; // Aq tb
 
-    switch (menu){
+    boolean ContaCriada = false; //Verifica se a conta foi devidamente criada
+    float saque, deposito, transferencia; // criar dentro do case
+    float saldo = 0;
+    int menu = 0; //Contador para verificar qual ação a ser realizada
+    String nome = ""; //Nome do cliente
+    String contaTransferida = ""; //Conta que será transferida
+
+    //Pede a criação de conta no banco Trilegal
+    while (ContaCriada != true || menu != 2) {
+      System.out.println("---------- Banco Trilegal ----------");
+      System.out.println("Seja bem vindo ao Banco Trilegal! \n");
+
+      System.out.println("Para realizar uma ação, digite uma das opções abaixo:");
+      System.out.println("--> ATENÇÃO, PARA REALIZAR QUALQUER PROCEDIMENTO, POR FAVOR, CRIE UMA CONTA");
+      System.out.println("1 - Cadastrar Conta");
+      System.out.println("2 - Sair");
+
+      System.out.print("Digite o número da opção desejada: ");
+      menu = sc.nextInt();
+
+      switch (menu) {
         case 1:
-          //Ainda tem que pedir para o usuário a quantidade do saque
-          System.out.println("Saque" + (saldo - saque));
-          float saque = sc.nextFloat();
+          System.out.print("Digite seu nome: ");
+          nome = sc.next();
+          //sc.next(); //Para debug, pois fica um "enter" no final da variável nome
+
+          System.out.println("Seja bem vindo, " + nome + "! Seu saldo é de R$ 0,00"); 
+          saldo = 0;
+          ContaCriada = true;
           break;
-      case 2:
-        //Do depósito
-        System.out.println("Deposito" + (saldo + deposito));
-         break;
-      case 3:
-        //E da transferencia
-        System.out.println("Transferencia");
-        break;
-        
+      }
     }
-     
-     }
+
+    System.out.println(" Obrigado por realizar seu cadastro! :)");
+
+    //Inicia o menu
+    while (menu != 6) { //6 é o número que coloquei para ser o fim do programa
+      System.out.println("\n---------- Banco Trilegal ----------");
+      System.out.println("Seja bem vindo, " + nome + ",  ao Banco Trilegal! \n");
+
+      System.out.println("Para realizar uma ação, digite uma das opções abaixo:");
+      System.out.println("1 - Sacar");
+      System.out.println("2 - Saldo");
+      System.out.println("3 - Depositar");
+      System.out.println("4 - Transferência");
+      System.out.println("5 - Cadastrar um novo usuário");
+      System.out.println("6 - Sair");
+
+      System.out.print("Digite o número da opção desejada: ");
+      menu = sc.nextInt();
+
+      System.out.println(" ");
+
+      switch (menu) {
+        case 1: //Sacar
+          System.out.print("Digite o valor do saque: R$");
+          saque = sc.nextFloat();
+          saldo =- saque; //Retira o valor do saldo
+
+          System.out.println("Saque realizado com sucesso! Seu saldo atual é de R$" + saldo);
+          break;
+
+        case 2: //Mostra o saldo
+          System.out.println("Seu saldo é de: R$" + saldo);
+          break;
+
+        case 3://Depositar
+          // Do depósito
+          System.out.print("Digite o valor do depósito: R$");
+          deposito = sc.nextFloat();
+
+          saldo =+ deposito; //Adiciona o valor ao saldo
+          System.out.println("Depósito realizado com sucesso! Seu saldo atual é de R$" + saldo);
+          break;
+
+        case 4://Transferência
+          // E da transferencia
+          System.out.print("Digite o valor da transferência: R$");
+          transferencia = sc.nextFloat();
+
+          System.out.println("Transferir para qual conta?");
+          contaTransferida = sc.next();
+
+          sc.next(); //Para debug, pois fica um "enter" no final 
+
+          saldo =- transferencia; //Retira o valor do saldo
+          System.out.println("Transferência realizada com sucesso! Seu saldo atual é de R$" + saldo);
+          break;
+
+        case 5: //Cadastra um novo usuário
+          //ATENÇÃO: Temos um problema aqui, como que vamos identificar os clientes? temos que usar vetores ou matrizes para organizar os clientes, então poderia ser por números..
+          //Então, tem que implementar isso, e deve ficar dividido: Olá cliente número: NÚUMERO_CLIENTE, seja bem vindo NOME
+          System.out.print("Digite seu nome: ");
+          nome = sc.next();
+          sc.next(); //Para debug, pois fica um "enter" no final da variável nome
+
+          System.out.println("Seja bem vindo, " + nome + "! Seu saldo é de R$ 0,00"); 
+          saldo = 0;
+          ContaCriada = true;
+          break;
+      }
+
+      System.out.println(" ");
+    }
+
+    sc.close();
+    System.out.println("Obrigado por utilizar o Banco Trilegal!");
+
+  }
 }
 
-//Possuir um menu e submenus utilizando switch case;
-//O programa só poderá encerrar após o usuário informar;
-//Utilizar vetores ou matrizes para registrar as informações. Não pode usar ArrayList ou outras classes de objetos Java;
-//Utilizar estruturas de controle e repetição para realizar operações do banco;
-// O usuário cliente deverá poder: sacar, depositar, transferir de uma conta para outra conta de outro cliente já cadastrado.
-//O sistema deve visualizar o saldo de cada cliente cadastrado.
+// Possuir um menu e submenus utilizando switch case;
+// O programa só poderá encerrar após o usuário informar;
+// Utilizar vetores ou matrizes para registrar as informações. Não pode usar
+// ArrayList ou outras classes de objetos Java;
+// Utilizar estruturas de controle e repetição para realizar operações do banco;
+// O usuário cliente deverá poder: sacar, depositar, transferir de uma conta
+// para outra conta de outro cliente já cadastrado.
+// O sistema deve visualizar o saldo de cada cliente cadastrado.
