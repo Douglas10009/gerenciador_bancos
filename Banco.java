01f63d85f107ad76606a9c2177cd1b258f856e34
Douglas10009 <douglasevertonev@gmail.com>
2022-04-22 17:27:33 -0300

    Alterações 2

--- Banco.java
+++ Banco.java
@@ -5,13 +5,19 @@ public class Banco {
     Scanner sc = new Scanner(System.in);
 
     boolean ContaCriada = false; // Verifica se a conta foi devidamente criada
-    int [] conta = new int[10]; // Cria um vetor de 10 posições com o número das contas
-    String [] nome = new String[10]; // Nome do cliente
+    int[] conta = new int[10]; // Cria um vetor de 10 posições com o número das contas
+    int i = 0; // Contador para o vetor
+    int j = 1; // Contador para o vetor
+    float esseSaldo = 0; // Saldo da conta
+    float n_conta_transferida = 0; // Número da conta a ser transferida
+
+    // String [] nome = new String[10]; // Nome do cliente
 
     float saque, deposito, transferencia;
-    float [] saldo = new float[10];
+    int essaConta = 0;
+    float[] saldo = new float[10];
     int menu = 0; // Contador para verificar qual ação a ser realizada
-    String contaTransferida = ""; // Conta que será transferida
+    int contaTransferida = 0; // Conta que será transferida
 
     // Pede a criação de conta no banco Trilegal
     while (ContaCriada != true && menu != 2) {
@@ -28,12 +34,16 @@ public class Banco {
 
       switch (menu) {
         case 1:
-          System.out.print("Digite seu nome: ");
-          nome = sc.next();
+          conta[0] = 1;
+          essaConta = conta[0]; // Define uma variável para a conta que será criada
+          esseSaldo = saldo[0]; // Define uma variável para o saldo da conta que será criada
+          // System.out.print("Digite seu nome: ");
+          // nome = sc.next();
           // sc.next(); //Para debug, pois fica um "enter" no final da variável nome
 
-          System.out.println("Seja bem vindo, " + nome + "! Seu saldo é de R$ 0,00 \n");
-          saldo = 0;
+          System.out
+              .println("Seja bem vindo, seu número da conta é: " + conta[0] + "! Seu saldo é de R$" + esseSaldo + "\n");
+          // saldo = 0;
           ContaCriada = true;
           break;
       }
@@ -46,7 +56,7 @@ public class Banco {
       // Inicia o menu
       while (menu != 6) { // 6 é o número que coloquei para ser o fim do programa
         System.out.println("\n---------- Banco Trilegal ----------");
-        System.out.println("Seja bem vindo, " + nome + ",  ao Banco Trilegal! \n");
+        System.out.println("Seja bem vindo, conta número " + essaConta + ",  ao Banco Trilegal! \n");
 
         System.out.println("Para realizar uma ação, digite uma das opções abaixo:");
         System.out.println("1 - Sacar");
@@ -64,16 +74,20 @@ public class Banco {
         switch (menu) {
           case 1: // Sacar
 
-            //Verificar se tem saldo para sacar
+            // Verificar se tem saldo para sacar
             System.out.print("Digite o valor do saque: R$");
             saque = sc.nextFloat();
-            saldo = -saque; // Retira o valor do saldo
+            if (saque > esseSaldo) {
+              System.out.println("\nSaldo insuficiente!\n");
+            } else {
+              esseSaldo = -saque; // Retira o valor do saldo
+              System.out.println("Saque realizado com sucesso! Seu saldo atual é de R$" + esseSaldo);
+            }
 
-            System.out.println("Saque realizado com sucesso! Seu saldo atual é de R$" + saldo);
             break;
 
           case 2: // Mostra o saldo
-            System.out.println("Seu saldo é de: R$" + saldo);
+            System.out.println("Seu saldo é de: R$" + esseSaldo);
             break;
 
           case 3:// Depositar
@@ -81,8 +95,8 @@ public class Banco {
             System.out.print("Digite o valor do depósito: R$");
             deposito = sc.nextFloat();
 
-            saldo = +deposito; // Adiciona o valor ao saldo
-            System.out.println("Depósito realizado com sucesso! Seu saldo atual é de R$" + saldo);
+            esseSaldo = +deposito; // Adiciona o valor ao saldo
+            System.out.println("Depósito realizado com sucesso! Seu saldo atual é de R$" + esseSaldo);
             break;
 
           case 4:// Transferência
@@ -90,13 +104,36 @@ public class Banco {
             System.out.print("Digite o valor da transferência: R$");
             transferencia = sc.nextFloat();
 
-            System.out.println("Transferir para qual conta?");
-            contaTransferida = sc.next();
+            if (transferencia > esseSaldo) {
+              System.out.println("\nSaldo insuficiente!\n");
+            } else {
+              System.out.print("Transferir para qual conta? ");
+              contaTransferida = sc.nextInt();
 
-            sc.next(); // Para debug, pois fica um "enter" no final
+              if (contaTransferida == essaConta) {
+                System.out.println("Você não pode transferir para a mesma conta!");
 
-            saldo = -transferencia; // Retira o valor do saldo
-            System.out.println("Transferência realizada com sucesso! Seu saldo atual é de R$" + saldo);
+              }
+              if (contains(conta, contaTransferida)) {
+                esseSaldo = -transferencia; // Retira o valor do saldo da conta que será transferida
+                n_conta_transferida = conta[contaTransferida];
+                n_conta_transferida = +transferencia; // Adiciona o valor ao saldo da conta que receberá a transferência
+                System.out.println("Transferência realizada com sucesso! Seu saldo atual é de R$" + esseSaldo);
+
+              } else {
+                System.out.println("Conta não encontrada!");
+              }
+            }
+
+            // }else {
+            // saldo[i] =- transferencia; // Retira o valor do saldo
+            // saldo[j] =+ transferencia; // Adiciona o valor ao saldo
+            // System.out.println("Transferência realizada com sucesso! Seu saldo atual é de
+            // R$" + saldo);
+
+            // }
+
+            // }
             break;
 
           case 5: // Cadastra um novo usuário
@@ -105,12 +142,21 @@ public class Banco {
             // ser por números..
             // Então, tem que implementar isso, e deve ficar dividido: Olá cliente número:
             // NÚUMERO_CLIENTE, seja bem vindo NOME
-            System.out.print("Digite seu nome: ");
-            nome = sc.next();
-            sc.next(); // Para debug, pois fica um "enter" no final da variável nome
 
-            System.out.println("Seja bem vindo, " + nome + "! Seu saldo é de R$ 0,00");
-            saldo = 0;
+            // System.out.print("Digite seu nome: ");
+            // nome = sc.next();
+            // sc.next(); // Para debug, pois fica um "enter" no final da variável nome
+
+            // Preciso de uma variável que defina
+            i++;
+            j++;
+
+            conta[i] = j;
+            essaConta = conta[i]; // Define uma variável para a conta que será criada
+
+            System.out.println("Seja bem vindo, o número de sua conta é: " + essaConta + ", e seu saldo é de R$ 0,00");
+            saldo[i] = 0;
+            esseSaldo = saldo[i]; // Define uma variável para o saldo da conta que será criada
             ContaCriada = true;
             break;
         }
@@ -118,15 +164,25 @@ public class Banco {
         System.out.println(" ");
       }
 
-      
       System.out.println("Obrigado por utilizar o Banco Trilegal!");
-      
+
     } else {
       System.out.println("Ocorreu um erro ao criar sua conta!");
 
     }
 
-  sc.close();
+    sc.close();
+  }
+
+  public static boolean contains(final int[] arr, final int key) {
+    boolean found = false;
+
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == key) {
+        found = true;
+      }
+    }
+    return found;
   }
 }
 
